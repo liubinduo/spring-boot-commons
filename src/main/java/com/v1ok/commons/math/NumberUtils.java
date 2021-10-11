fcbe89999e9ec73920af29460993e29894dc6400
liubinduo <liubinduo@v1ok.com>
2021-10-11 14:33:25 +0800

    添加 NumberUtils 中关于 BigDecimal 的加减乘除运算工具方法

--- src/main/java/com/v1ok/commons/math/NumberUtils.java
+++ src/main/java/com/v1ok/commons/math/NumberUtils.java
@@ -6,6 +6,9 @@ import org.apache.commons.lang3.ArrayUtils;
 
 public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils {
 
+  public static BigDecimal add(Number... numbers) {
+    return add(3, RoundingMode.HALF_UP, numbers);
+  }
 
   public static BigDecimal add(final int scale, final RoundingMode roundingMode,
       Number... numbers) {
@@ -24,6 +27,10 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils {
       }
     }
     return NumberUtils.toScaledBigDecimal(bigDecimal, scale, roundingMode);
+  }
+
+  public static BigDecimal subtract(Number... numbers) {
+    return subtract(3, RoundingMode.HALF_UP, numbers);
   }
 
   public static BigDecimal subtract(final int scale, final RoundingMode roundingMode,
@@ -45,6 +52,10 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils {
     return NumberUtils.toScaledBigDecimal(bigDecimal, scale, roundingMode);
   }
 
+  public static BigDecimal multiply(Number... numbers) {
+    return multiply(3, RoundingMode.HALF_UP, numbers);
+  }
+
   public static BigDecimal multiply(final int scale, final RoundingMode roundingMode,
       Number... numbers) {
     BigDecimal bigDecimal = null;
@@ -62,6 +73,10 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils {
       }
     }
     return NumberUtils.toScaledBigDecimal(bigDecimal, scale, roundingMode);
+  }
+
+  public static BigDecimal divide(Number... numbers) {
+    return divide(3, RoundingMode.HALF_UP, numbers);
   }
 
   public static BigDecimal divide(final int scale, final RoundingMode roundingMode,
@@ -83,5 +98,51 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils {
     return NumberUtils.toScaledBigDecimal(bigDecimal, scale, roundingMode);
   }
 
+  /**
+   * 大于判断，num1 > num2 返回 True
+   *
+   * @param num1 比较数
+   * @param num2 比较数
+   * @return num1 > num2 返回 True 否则返回 False
+   */
+  public static boolean greater(BigDecimal num1, BigDecimal num2) {
+    int result = num1.compareTo(num2);
+    return result >= 1;
+  }
 
+  /**
+   * 小于判断，num1 < num2 返回 True
+   *
+   * @param num1 比较数
+   * @param num2 比较数
+   * @return num1 > num2 返回 True 否则返回 False
+   */
+  public static boolean less(BigDecimal num1, BigDecimal num2) {
+    int result = num1.compareTo(num2);
+    return result <= -1;
+  }
+
+  /**
+   * 等于判断，num1 == num2 返回 True
+   *
+   * @param num1 比较数
+   * @param num2 比较数
+   * @return num1 > num2 返回 True 否则返回 False
+   */
+  public static boolean equal(BigDecimal num1, BigDecimal num2) {
+    int result = num1.compareTo(num2);
+    return result == 0;
+  }
+
+  /**
+   * 区间比较
+   *
+   * @param num 操作数
+   * @param min 最少值
+   * @param max 最大值
+   * @return if(min < num < = max) true
+   */
+  public static boolean between(BigDecimal num, BigDecimal min, BigDecimal max) {
+    return greater(num, min) && (less(num, max) || equal(num, max));
+  }
 }
