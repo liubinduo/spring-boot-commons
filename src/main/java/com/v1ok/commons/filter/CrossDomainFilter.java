447f2b4c157d6c03d0b56c06f87a1ccac139022e
liubinduo <liubinduo@v1ok.com>
2021-10-26 10:04:29 +0800

    update spring boot to 2.5.5

--- src/main/java/com/v1ok/commons/filter/CrossDomainFilter.java
+++ src/main/java/com/v1ok/commons/filter/CrossDomainFilter.java
@@ -1,6 +1,7 @@
 package com.v1ok.commons.filter;
 
 import java.io.IOException;
+import java.util.Collection;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -22,18 +23,26 @@ public class CrossDomainFilter implements Filter {
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
 
+    chain.doFilter(request, response);
+
     log.info("添加跨域请求消息头");
 
     HttpServletResponse httpResponse = (HttpServletResponse) response;
 
     httpResponse.setHeader("Access-Control-Allow-Origin", "*");
-    httpResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    httpResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     httpResponse.setHeader("Access-Control-Max-Age", "3600");
     httpResponse.setHeader("Access-Control-Allow-Headers",
-        "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With");
+        "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With, X-token, token");
     httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
 
-    chain.doFilter(request, response);
+    if (log.isInfoEnabled()) {
+      Collection<String> headerNames = httpResponse.getHeaderNames();
+      headerNames.stream().forEach(headerName -> {
+        System.out.println(headerName + ":" + httpResponse.getHeader(headerName));
+      });
+    }
+
   }
 
   @Override
